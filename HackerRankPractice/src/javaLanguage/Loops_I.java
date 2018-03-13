d6eabc284737c8341922bfaa47882c8709aa39a5
galandepmayur12 <galandepmayur@gmail.com>
2018-03-13 18:23:09 +0530

    resolved a warning in if_else and 5th problem of loop

---
+++ HackerRankPractice/src/javaLanguage/Loops_I.java
@@ -0,0 +1,21 @@
+package javaLanguage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Loops_I {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(num + " x " + i + " = " + num*i);
+		}
+		
+	}
+
+}
